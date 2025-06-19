51947ef689484888449ba0be98642835b58a1c70
Jayden <huangwq0518@163.com>
2025-06-19 10:25:56 +0100

    update

--- src/main/java/com/hwq/reggie/common/Update.java
+++ src/main/java/com/hwq/reggie/common/Update.java
@@ -1,5 +1,11 @@
 package com.hwq.reggie.common;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
+
 public class Update {
 
+    public void show() {
+        System.out.println("hello");
+
+    }
 }
